21990a8d4d02e165156a31132a03a543417c280c
Evrim Çolakoğlu <p4rgelievc0p4sha305190328@outlook.com>
2025-11-29 23:22:42 +0300

    Add entry point for TAI simulation software

---
+++ src/main.py
@@ -0,0 +1,3 @@
+# [ENG]: TAI Simulation Software / [TR]: TAI Yazılım Simülasyonu
+# [ENG]: Entry point for the simulation / [TR]: Simülasyon için giriş noktası
+print("TAI Simulation initializing...") 
